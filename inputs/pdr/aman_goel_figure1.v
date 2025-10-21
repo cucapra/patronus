a2c20b37e6c382325f6a2e6cca9ab6b86df744f2
Kevin Läufer <laeufer@cornell.edu>
2025-10-21 13:58:43 -0400

    inputs: pdr paper example

---
+++ inputs/pdr/aman_goel_figure1.v
@@ -0,0 +1,13 @@
+`define W 16
+module example(clk);
+  input clk;
+  reg [`W-1:0] u = `W'd1;
+  reg [`W-1:0] v = `W'd1;
+  always @(posedge clk) begin
+    if (u < v) u <= u + v;
+    else u <= v + `W'd1;
+    v <= v + `W'd1;
+  end
+
+  always @(*) assert ((u + v) != `W'd1);
+endmodule
