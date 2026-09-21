e15154c112e6fcac5c874b3a497192f67e0f63c0
Kevin Läufer <laeufer@cornell.edu>
2026-09-21 09:02:15 -0400

    expr: expose ExprTransformMode

--- patronus/src/expr.rs
+++ patronus/src/expr.rs
@@ -28,6 +28,6 @@ pub use serialize::SerializableIrNode;
 pub(crate) use serialize::{serialize_expr, serialize_expr_ref};
 pub(crate) use simplify::simplify;
 pub use simplify::{Simplifier, as_equality, simplify_single_expression};
-pub use transform::simple_transform_expr;
-pub(crate) use transform::{ExprTransformMode, do_transform_expr};
+pub(crate) use transform::do_transform_expr;
+pub use transform::{ExprTransformMode, simple_transform_expr};
 pub use types::{TypeCheck, TypeCheckError};
